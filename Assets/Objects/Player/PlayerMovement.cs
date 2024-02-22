b6fddaf6e9c00b2b25910d9f8420c30239e91aa2
Ashobot <42091725+Ashobot@users.noreply.github.com>
2024-02-22 01:18:04 +0100

    Changement de direction smooth pendant le dash (fini)

--- Assets/Objects/Player/PlayerMovement.cs
+++ Assets/Objects/Player/PlayerMovement.cs
@@ -7,31 +7,40 @@ using UltimateAttributesPack;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    // --------------------- //
     // ----- Variables ----- //
+    // --------------------- //
 
     // Components
     PlayerController _playerController;
     InputsManager _inputsManager;
 
     Rigidbody2D _rb;
+    SpriteRenderer _sr;
 
-    // Values
+    // Movement
     [Title("Movement", "white", "light blue")]
 
     [SerializeField, MinValue(0)] float _movementSpeed = 200f;
+    [SerializeField] bool _smoothChangeDirection;
+    [SerializeField, ShowIf(nameof(_smoothChangeDirection), true), MinValue(0)] float _moveChangeDirectionSpeed = 5f;
     bool _isMoving;
 
+    // Dash
     [Title("Dash", "white", "blue")]
 
-    [SerializeField] bool _canMoveDuringDash = true;
-    [SerializeField, ShowIf(nameof(_canMoveDuringDash), true)] float _dashMoveTime = 0.75f;
-    [SerializeField, ShowIf(nameof(_canMoveDuringDash), true)] float _dashMoveMaxAngle = 30f;
+    [SerializeField] bool _canChangeDirectionDuringDash = true;
+    [SerializeField, ShowIf(nameof(_canChangeDirectionDuringDash), true)] Transform _currentRotator;
+    [SerializeField, ShowIf(nameof(_canChangeDirectionDuringDash), true)] Transform _targetRotator;
+    [SerializeField, ShowIf(nameof(_canChangeDirectionDuringDash), true), MinValue(0)] float _dashChangeDirectionSpeed = 0.75f;
     [Space]
     [SerializeField, MinValue(0)] float _dashCooldown = 1f;
     [Line("blue")]
     [SerializeField] AnimationCurve _dashCurve;
     [SerializeField, MinValue(0)] float _dashLenght = 3f;
     [SerializeField, MinValue(0)] float _dashDuration = 0.25f;
+    [Space]
+    [SerializeField] Color _colorWhenDash;
     float _dashTimer = 0f;
     bool _canDash = true;
     bool _isDashing = false;
@@ -40,13 +49,18 @@ public class PlayerMovement : MonoBehaviour
     Vector2 _lastDashTargetEndPos;
     Vector2 _dashVelocityRef;
 
+    // Debug
     [Title("Debug", "white", "grey")]
 
     [SerializeField] bool _showDebug;
-    [SerializeField, ShowIf(nameof(_showDebug), true)] Color _dashLineDebugColor;
-    [SerializeField, ShowIf(nameof(_showDebug), true)] Color _dashTargetPosDebugColor;
+    [SerializeField, ShowIf(nameof(_showDebug), true)] Color _currentForwardDebugColor;
+    [SerializeField, ShowIf(nameof(_showDebug), true)] Color _targetForwardDebugColor;
+    [SerializeField, ShowIf(nameof(_showDebug), true)] float _forwardDebugLenght;
+    Color _normalColor;
 
+    // --------------------- //
     // ----- Functions ----- //
+    // --------------------- //
 
     private void Awake()
     {
@@ -56,12 +70,19 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _inputsManager = _playerController.InputManager;
+
         _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+
+        _normalColor = _sr.color;
     }
 
     private void Update()
     {
+        ManageRotators();
         ManageDash();
+
+        ManageColor();
     }
 
     private void FixedUpdate()
@@ -72,20 +93,44 @@ public class PlayerMovement : MonoBehaviour
             Dash();
     }
 
-    // Movements functions
+    // ----- Rotations ----- //
+
+    void ManageRotators()
+    {
+        // Set target rotator rotation with stick input
+        if (_inputsManager.MoveInput != Vector2.zero)
+            _targetRotator.up = _inputsManager.MoveInput;
+        else
+            _targetRotator.up = _currentRotator.up;
+
+        // Smooth rotation from current to target
+        if (_isDashing && _canChangeDirectionDuringDash) // If we dash and we can change direction during it
+            _currentRotator.rotation = Quaternion.RotateTowards(_currentRotator.rotation, _targetRotator.rotation, _dashChangeDirectionSpeed * Time.deltaTime); 
+        else if (_isMoving && _smoothChangeDirection) // If we move and smooth change direction is activated
+            _currentRotator.rotation = Quaternion.RotateTowards(_currentRotator.rotation, _targetRotator.rotation, _moveChangeDirectionSpeed * Time.deltaTime);
+        else // If movement and dash rotation are both not smooth
+            _currentRotator.rotation = _targetRotator.rotation;
+    }
+
+    // ----- Movement ----- //
 
     private void Move()
     {
+        // Move only if not dashing and stick input is not 0
         if(_inputsManager.MoveInput != Vector2.zero && !_isDashing)
         {
             _isMoving = true;
-            _rb.MovePosition((Vector2)transform.position + (_inputsManager.MoveInput * _movementSpeed * Time.deltaTime));
+     
+            if(_smoothChangeDirection) // Smooth change direction when moving if it's activated
+                _rb.MovePosition((Vector2)transform.position + ((Vector2)_currentRotator.up * _movementSpeed * Time.deltaTime));
+            else
+                _rb.MovePosition((Vector2)transform.position + (_inputsManager.MoveInput * _movementSpeed * Time.deltaTime));
         }
         else
-        {
             _isMoving = false;
-        }
     }
+
+    // ----- Dash ----- //
 
     void ManageDash()
     {
@@ -103,7 +148,7 @@ public class PlayerMovement : MonoBehaviour
     void StartDash()
     {
         _startDashPos = transform.position;
-        _endDashPos = _startDashPos + (_inputsManager.MoveInput * _dashLenght);
+        _endDashPos = _startDashPos + (_inputsManager.MoveInput * _dashLenght); // Calculate end dash position
         _isDashing = true;
         _canDash = false;
     }
@@ -114,33 +159,13 @@ public class PlayerMovement : MonoBehaviour
         // Lerp velocity to dash speed if timer is not finished
         if(_dashTimer < _dashDuration)
         {
-            //if (_canMoveDuringDash)
-            //{
-            //    Vector2 _targetEndDashPos;
-            //    if(_inputsManager.MoveInput != Vector2.zero)
-            //    {
-            //        _targetEndDashPos = _startDashPos + (_inputsManager.MoveInput * _dashLenght);
-            //        _lastDashTargetEndPos = _targetEndDashPos;
-            //    }
-            //    else
-            //    {
-            //        _targetEndDashPos = _lastDashTargetEndPos;
-            //    }
+            // Smooth change end dash position if it's activated
+            if (_canChangeDirectionDuringDash)
+            {
+                _endDashPos = _startDashPos + ((Vector2)_currentRotator.up * _dashLenght);
+            }
 
-            //    Debug.Log(Vector2.Angle(_endDashPos, _targetEndDashPos));
-            //    float _currentAngle = Vector2.Angle(_endDashPos, _targetEndDashPos);
-            //    if (_currentAngle < _dashMoveMaxAngle)
-            //    {
-            //        // Smooth current dash end position to target dash end position
-            //        _endDashPos = Vector2.SmoothDamp(_endDashPos, _targetEndDashPos, ref _dashVelocityRef, _dashMoveTime);
-            //    }
-            //    else
-            //    {
-            //        Vector2 _angleTargetEndDashPos = (Vector2)(Quaternion.Euler(0, 0, _dashMoveMaxAngle) * Vector2.up);
-            //        _endDashPos = Vector2.SmoothDamp(_endDashPos, _angleTargetEndDashPos, ref _dashVelocityRef, _dashMoveTime);
-            //    }
-            //}
-
+            // Calculate and set position of player
             Vector2 currentDashPosition = Vector2.Lerp(_startDashPos, _endDashPos, _dashCurve.Evaluate(_dashTimer / _dashDuration));
             _rb.MovePosition(currentDashPosition);
 
@@ -166,6 +191,17 @@ public class PlayerMovement : MonoBehaviour
         _canDash = true;
     }
 
+    // ----- Color ----- //
+
+    void ManageColor()
+    {
+        // Manage sprite color
+        if (_isDashing)
+            _sr.color = _colorWhenDash;
+        else
+            _sr.color = _normalColor;
+    }
+
     // ----- Debug ----- //
 
     private void OnDrawGizmos()
@@ -173,13 +209,12 @@ public class PlayerMovement : MonoBehaviour
         if (!_showDebug)
             return;
 
-        if(_isDashing && _canMoveDuringDash)
-        {
-            Gizmos.color = _dashLineDebugColor;
-            Gizmos.DrawLine(_startDashPos, _endDashPos);
+        // Draw current rotation line
+        Gizmos.color = _currentForwardDebugColor;
+        Gizmos.DrawLine(transform.position, transform.position + (_currentRotator.up * _forwardDebugLenght));
 
-            Gizmos.color = _dashTargetPosDebugColor;
-            Gizmos.DrawSphere(_lastDashTargetEndPos, 0.3f);
-        }
+        // Draw target rotation line
+        Gizmos.color = _targetForwardDebugColor;
+        Gizmos.DrawLine(transform.position, transform.position + (_targetRotator.up * _forwardDebugLenght));
     }
 }
